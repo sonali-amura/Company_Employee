1e51bb9f9deff882cd39a5fad1e288fca4c822b5
sonali-amura <sonali@amuratech.com>
2016-08-30 13:00:37 +0530

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,4 +7,18 @@ class Employee < ActiveRecord::Base
 	validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 	validates_format_of :name,:with => /\A[A-Za-z ]+\Z/
 	validates_numericality_of :salary,:greater_than=> 0
+	
+	def self.query1
+		@emp=Employee.includes(:addresses).where(:addresses=>{city:"Pune"}).all
+ 	end
+ 	def self.query2
+  		Employee.includes(:addresses).where(:name=>"Mr Bhawalkar").pluck("name,email,streat,city,pincode")
+	end
+ 	def self.query3
+ 		Employee.select("name,salary").where("salary>? AND salary <?",20,50)
+ 	end
+ 	def self.validate?
+ 		e=Employee.create(name:"Sonali",email:"xyzgmail.com",phone:980,salary:0,designation:"tech",company_id:2)
+		e.errors.messages
+ 	end
 end
